aa77209f15d9e6e2649054de293e9d131251d5f4
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-10-17 19:50:07 +0530

    Update WOF_testing.py

--- WheelOfFortune/WOF_testing.py
+++ WheelOfFortune/WOF_testing.py
@@ -1,17 +1,11 @@
-import openpyxl
-import pandas as pd
-
-f=open("Database//ListOfPrizes.txt","w")
+f=open("test.txt","w")
 new=[]
 while True:
-    new.append(input("Enter new prize: "))
-    r=input("Continue? y/n")
+    new.append(input("Enter new prize: ")+"\n")
+    r=input("Continue? y/n ")
     if r=='n' or r=='N':
         break
     else:
         continue
-file.writelines(new)
-
-d = pd.DataFrame(new)
-d.to_excel('woftest.xlsx', sheet_name='Sheet1',index=False, header=False)
-print(d)
+f.writelines(new)
+f.close()
